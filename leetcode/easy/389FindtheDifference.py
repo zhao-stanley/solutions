97afed0cd35950ee8b77403fe38977e7f444b6e1
Stanley Zhao <stanleyhzhao@gmail.com>
2023-12-03 14:06:56 -0500

    Update 389FindtheDifference.py

--- leetcode/easy/389FindtheDifference.py
+++ leetcode/easy/389FindtheDifference.py
@@ -1,3 +1,7 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        pass
+        diff = t
+        for char in s:
+            if char in diff:
+                diff = diff.replace(char, "", 1)
+        return diff
